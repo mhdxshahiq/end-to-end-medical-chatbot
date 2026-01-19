3de87f26846cbb63cf7755a15c64b3b45580938b
mhdxshahiq <muhammedxshahiq@gmail.com>
2026-01-19 15:23:50 +0530

    trail done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="end_to_end_medical_chatbot",
+    version="0.1.0",
+    author="shahiq",
+    author_email="muhammedxshahiq@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+),
+# description="An end-to-end medical chatbot application",
